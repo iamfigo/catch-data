cdbc96ebaf33f6aa4acfa3bec8932185e8fc057f
huit <iamfigo@foxmail.com>
2016-07-20 11:07:32 +0800

    bat脚本

---
+++ catch-data/src/main/resources/catch.bat
@@ -0,0 +1,4 @@
+@echo off
+set base_dir="%~dp0"
+pushd %base_dir%
+java -jar catch-data-jar-with-dependencies.jar
---
+++ catch-data/src/main/resources/parse.bat
@@ -0,0 +1,4 @@
+@echo off
+set base_dir="%~dp0"
+pushd %base_dir%
+java - cp catch - data- jar -with - dependencies.jar com.huit.util.ParseData
